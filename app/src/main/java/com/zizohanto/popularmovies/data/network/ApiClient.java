b53f1e4513f4a0b13710a3fe5fdb35c22081569f
Zizoh Anto <zizohanto@hotmail.com>
2018-08-31 13:26:16 +0100

    feature: Use OkHttp interceptor to log request and response information

--- app/src/main/java/com/zizohanto/popularmovies/data/network/ApiClient.java
+++ app/src/main/java/com/zizohanto/popularmovies/data/network/ApiClient.java
@@ -1,20 +1,33 @@
 package com.zizohanto.popularmovies.data.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
-    private static Retrofit retrofit = null;
+    private static ApiInterface retrofit = null;
+    private static OkHttpClient client = null;
 
 
-    public static Retrofit getClient() {
+    public static ApiInterface getClient() {
+
+        HttpLoggingInterceptor logger = new HttpLoggingInterceptor();
+        logger.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        client = new OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build();
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+                    .build()
+                    .create(ApiInterface.class);
         }
         return retrofit;
     }
