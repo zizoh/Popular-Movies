8a7b49b84bedf2695978215529f09f6009156fbb
Zizoh Anto <zizohanto@hotmail.com>
2018-09-05 10:10:40 +0100

    feature: Remove okHttp interceptor since logging already provided by retrofit

--- app/src/main/java/com/zizohanto/popularmovies/data/network/ApiClient.java
+++ app/src/main/java/com/zizohanto/popularmovies/data/network/ApiClient.java
@@ -1,7 +1,5 @@
 package com.zizohanto.popularmovies.data.network;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,22 +7,12 @@ public class ApiClient {
 
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
     private static ApiInterface retrofit = null;
-    private static OkHttpClient client = null;
-
 
     public static ApiInterface getClient() {
-
-        HttpLoggingInterceptor logger = new HttpLoggingInterceptor();
-        logger.setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-        client = new OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build();
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
                     .create(ApiInterface.class);
